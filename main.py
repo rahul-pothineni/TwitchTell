7afe48b34b1dcaa8d511f8752605ba80dc94c47b
rahul-pothineni <rahulpot07@gmail.com>
2026-04-25 19:29:53 -0400

    Feat: Added both kafka topics( row producers and sentiment anyalysis consumer), adding hugging face sentiment anylysis.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+"""
+Filename: main.py
+Author: Rahul Pothineni
+Date: 2026-04-25
+Version: 1.0.0
+Description: This module is used to open a websocket connection to the twitch chat 
+from the twitch API and produce the messages to a kafka topic for further processing.
+"""
+
 import os
-from pathlib import Path
+import json
 
 from dotenv import load_dotenv
 
@@ -11,6 +20,8 @@ from twitchAPI.object.eventsub import ChannelChatMessageEvent
 from twitchAPI.type import AuthScope
 import asyncio
 import certifi
+from quixstreams import Application 
+
 os.environ["SSL_CERT_FILE"] = certifi.where()
 os.environ["SSL_CERT_DIR"] = os.path.dirname(certifi.where())
 
@@ -22,12 +33,15 @@ TARGET_CHANNEL = os.getenv('TARGET_CHANNEL')
 # dev user auth scopes
 SCOPES = [AuthScope.USER_READ_CHAT]
 
-# Handle chat messages
-async def on_chat(data: ChannelChatMessageEvent): 
-    e = data.event
-    print(f"[{e.broadcaster_user_login}] {e.chatter_user_login}: {e.message.text}")
+#kafka producer
+producer_app = Application(broker_address="localhost:9092")
 
-
+"""
+Error handling: all API keys
+Starts the twitch API and starts the websocket
+Listens for chat messages
+Produces the raw chat messages to a kafka topic
+"""
 async def run():
     twitch = await Twitch(APP_ID, APP_SECRET)
 
@@ -46,29 +60,49 @@ async def run():
     print(f"authed as: {me.login}")
     print(f"monitoring: {target.login} (id={target.id})")
 
-    # create eventsub websocket instance and start the client.
-    eventsub = EventSubWebsocket(twitch)
-    eventsub.start()
+    #defining the producers lifecycle; when the block is exited (user ctrl+c or 3600 seconds)
+    with producer_app.get_producer() as producer:
+        # Handle chat messages
+        async def on_chat(data: ChannelChatMessageEvent): 
+            e = data.event
+            producer_payload = {
+                "broadcaster_channel": e.broadcaster_user_login,
+                "sending_user": e.chatter_user_login,
+                "message": e.message.text,
+            }
+            produce_to_kafka(producer_payload)
 
-    # listen for chat messages
-    await eventsub.listen_channel_chat_message(
-        broadcaster_user_id=target.id,
-        user_id=me.id,
-        callback=on_chat,
-    )
+        #produce to kafka raw topic
+        def produce_to_kafka(producer_payload):
+            producer.produce(
+                topic = "twitch_chat",
+                key = TARGET_CHANNEL.encode("utf-8"),
+                value = json.dumps(producer_payload).encode("utf-8")
+            )
 
+        # create eventsub websocket instance and start the client.
+        eventsub = EventSubWebsocket(twitch)
+        eventsub.start()
 
-    # eventsub will run in its own process
-    # so lets just wait for user input before shutting it all down again
-    print("listening — press Ctrl+C to stop")
-    try:
-        while True:
-            await asyncio.sleep(3600)
-    except (KeyboardInterrupt, asyncio.CancelledError):
-        pass
-    finally:
-        await eventsub.stop()
-        await twitch.close()
+        # listen for chat messages
+        await eventsub.listen_channel_chat_message(
+            broadcaster_user_id=target.id,
+            user_id=me.id,
+            callback=on_chat,
+        )
+
+        # eventsub will run in its own process
+        # so lets just wait for user input before shutting it all down again
+        print("listening — press Ctrl+C to stop")
+        try:
+            while True:
+                await asyncio.sleep(3600)
+        except (KeyboardInterrupt, asyncio.CancelledError):
+            pass
+        finally:
+            producer.flush(timeout=5)
+            await eventsub.stop()
+            await twitch.close()
 
 if __name__ == "__main__":
     asyncio.run(run())
---
+++ sentiment_consumer.py
@@ -0,0 +1,40 @@
+from quixstreams import Application
+import json
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+
+"""
+Filename: sentiment_consumer.py
+Author: Rahul Pothineni
+Date: 2026-04-25
+Version: 1.0.0
+Description: This module is used to consume the raw chat messages from the kafka topic
+and produce the sentiment analysis to a kafka topic.
+"""
+#kafka consumer application
+app = Application(
+    broker_address="localhost:9092",
+    consumer_group="sentiment_consumer",
+    auto_offset_reset="earliest", #start from the beginning of the topic
+)
+
+#sentiment analysis model
+model_path = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+model = AutoModelForSequenceClassification.from_pretrained(model_path, from_tf=True)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+sentiment_task = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
+
+#kafka consumer with sentiment analysis
+with app.get_consumer() as consumer:
+    consumer.subscribe(["twitch_chat"])
+    while True:
+        msg = consumer.poll(1)
+        if msg is None:
+            continue
+        if msg.error() is not None:
+            print(f"Error: {msg.error()}")
+            continue
+        payload = json.loads(msg.value().decode("utf-8"))
+        print(payload["message"] + " - " + str(sentiment_task(payload["message"])[0]["score"]))
+
+        #sets the offset to the last message (we've read this message)
+        consumer.store_offsets(msg)
